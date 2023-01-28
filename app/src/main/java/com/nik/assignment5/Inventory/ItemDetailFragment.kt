845ee5c833d7740da26ecb4754f7cce9b55d5c98
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2023-01-28 22:22:54 +0600

    a

---
+++ app/src/main/java/com/nik/assignment5/Inventory/ItemDetailFragment.kt
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nik.assignment5
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.nik.assignment5.databinding.FragmentItemDetailBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.nik.assignment5.Inventory.InventoryApplication
+import com.nik.assignment5.Inventory.InventoryViewModel
+import com.nik.assignment5.Inventory.InventoryViewModelFactory
+import com.nik.assignment5.Inventory.data.Item
+import com.nik.assignment5.Inventory.data.getFormattedPrice
+
+/**
+ * [ItemDetailFragment] displays the details of the selected item.
+ */
+class ItemDetailFragment : Fragment() {
+    private val navigationArgs: ItemDetailFragmentArgs by navArgs()
+
+    private var _binding: FragmentItemDetailBinding? = null
+    private val binding get() = _binding!!
+    lateinit var item: Item
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    private fun bind(item: Item) {
+        binding.apply {
+            itemName.text = item.itemName
+            itemPrice.text = item.getFormattedPrice()
+            itemCount.text = item.quantityInStock.toString()
+            sellItem.isEnabled = viewModel.isStockAvailable(item)
+            sellItem.setOnClickListener { viewModel.sellItem(item) }
+            deleteItem.setOnClickListener { showConfirmationDialog() }
+            editItem.setOnClickListener { editItem() }
+        }
+    }
+
+    private fun editItem() {
+        val action = ItemDetailFragmentDirections.actionItemDetailFragmentToAddItemFragment(
+            getString(R.string.edit_fragment_title)
+        )
+        this.findNavController().navigate(action)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val id = navigationArgs.itemId
+        viewModel.retrieveItem(id).observe(this.viewLifecycleOwner) { selectedItem ->
+            item = selectedItem
+            bind(item)
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database.itemDao()
+        )
+    }
+
+    private fun deleteItem() {
+        this.viewModel.deleteItem(this.item)
+        this.findNavController().navigateUp()
+    }
+
+    private fun showConfirmationDialog() {
+        MaterialAlertDialogBuilder(this.requireContext())
+            .setPositiveButton(this.getString(R.string.yes)) { _, _ ->
+                this.deleteItem()
+            }
+            .show()
+    }
+
+    /**
+     * Called when fragment is destroyed.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+
+    }
+}
---
+++ app/src/main/java/com/nik/assignment5/Inventory/data/User.kt
@@ -0,0 +1,3 @@
+package com.nik.assignment5.Inventory.data
+
+data class User(val first_name: String, val last_name: String) {}
